17baaa632325a2ad0f811c6a9898d91dde129735
sumithreddy2022 <sumithreddy202@gmail.com>
2026-05-06 09:21:32 +0530

    Update QuantityMeasuementApp.java

--- src/QuantityMeasuementApp.java
+++ src/QuantityMeasuementApp.java
@@ -4,32 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootApplication
-class MeasurementApplication {
-    static class Feet{
-        private final double value;
+public class MeasurementApplication {
+	static class Feet{
+		private final double value;
 
-        public Feet(double value){
-            this.value = value;
-        }
+		public Feet(double value){
+			this.value = value;
+		}
 
-        @Override
-        public boolean equals(Object obj){
-            if(this == obj){
-                return true;
-            }
-            if(obj == null|| getClass() != obj.getClass()){
-                return false;
-            }
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-    }
+		@Override
+		public boolean equals(Object obj){
+			if(this == obj){
+				return true;
+			}
+			if(obj == null|| getClass() != obj.getClass()){
+				return false;
+			}
+			Feet other = (Feet) obj;
+			return Double.compare(this.value, other.value) == 0;
+		}
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(MeasurementApplication.class, args);
-        Feet f1 = new Feet(34.5);
-        Feet f2 = new Feet(34.5);
-        System.out.println(f1.equals(f2));
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MeasurementApplication.class, args);
+		Feet f1 = new Feet(34.5);
+		Feet f2 = new Feet(34.5);
+		System.out.println(f1.equals(f2));
+	}
 
 }
